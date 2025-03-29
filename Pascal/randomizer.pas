17998f84ebc4c966342febc38b45e033b824f668
Vitaliy Kazakevich <vitaliy.dev@icloud.com>
2025-03-29 12:51:29 +0100

    Adding parameters to the random number generation procedure specifying the range: from, to, how many

--- Pascal/randomizer.pas
+++ Pascal/randomizer.pas
@@ -1,33 +1,28 @@
 program Randomizer;
 
-const
-   size = 50;
-
 type 
-   IntArray = Array [1..size] of Integer;
+   IntArray = array of integer;
 
-var 
+var
    randomArr : IntArray;
-   i : Integer;
+   i : integer;
+   min, max, size : integer;
 
-procedure RandomIntArr(var arr: IntArray);   
+procedure RandomIntArr(var arr: IntArray; arrSize: integer; min: integer; max: integer);   
 
 begin
    Randomize;
 
-   for i:= 1 to size do
-      arr[i] := Random(100);
+   for i:= 1 to arrSize do
+      arr[i] := Random(max - min + 1) + min;
 end;
 
-procedure SortArr(var arr: IntArray);
+procedure SortArr(var arr: IntArray; arrSize: integer);
 
 var
-   arrSize : Integer;
-   i, j, temp : Integer;
+   i, j, temp : integer;
 
 begin
-   arrSize := Length(arr);
-
    for i := 1 to arrSize - 1 do
       for j := 1 to arrSize - 1 do
          if arr[j] > arr[j + 1] then begin
@@ -37,18 +32,45 @@ begin
          end;
 end;
 
-procedure DisplayArr(var arr: IntArray);   
+procedure DisplayArr(var arr: IntArray; arrSize: integer);   
 
 begin
-   for i := 1 to size do
+   Writeln('Result:');
+   for i := 1 to arrSize do
       write(randomArr[i], ' ');
-   writeln;
+   Writeln;
 end;
 
 begin
-   RandomIntArr(randomArr);
-   DisplayArr(randomArr);
+   Writeln('Write MIN random number (min 0):');
+   Readln(min);
 
-   SortArr(randomArr);
-   DisplayArr(randomArr);
+   if (min < 0) then begin
+      Writeln('Validation Error: MIN must be greater than 0.');
+      Halt (1);
+   end;
+
+   Writeln('Write MAX random number (min ', (min + 1),'):');
+   Readln(max);
+
+   if (min > max) then begin
+      Writeln('Validation Error: MAX must be greater than MIN.');
+      Halt (1);
+   end;
+
+   Writeln('Write count of random numbers (min 1):');
+   Readln(size);
+
+   if (size < 1) then begin
+      Writeln('Validation Error: Count of random numbers must be greater than 0.');
+      Halt (1);
+   end;
+
+   setLength(randomArr, size);
+
+   RandomIntArr(randomArr, size, min, max);
+   DisplayArr(randomArr, size);
+
+   SortArr(randomArr, size);
+   DisplayArr(randomArr, size);
 end. 
