9bbe480f72d4aba8f3c99ced34b4fd2b7338e6ad
Vitaliy Kazakevich <vitaliy.dev@icloud.com>
2025-03-29 11:57:13 +0100

    Procedure for sorting numbers

--- Pascal/randomizer.pas
+++ Pascal/randomizer.pas
@@ -4,7 +4,7 @@ const
    size = 50;
 
 type 
-   IntArray = Array [0..size] of Integer;
+   IntArray = Array [1..size] of Integer;
 
 var 
    randomArr : IntArray;
@@ -15,11 +15,40 @@ procedure RandomIntArr(var arr: IntArray);
 begin
    Randomize;
 
-   for i:= 0 to size do
+   for i:= 1 to size do
       arr[i] := Random(100);
+end;
+
+procedure SortArr(var arr: IntArray);
+
+var
+   arrSize : Integer;
+   i, j, temp : Integer;
+
+begin
+   arrSize := Length(arr);
+
+   for i := 1 to arrSize - 1 do
+      for j := 1 to arrSize - 1 do
+         if arr[j] > arr[j + 1] then begin
+            temp := arr[j];
+            arr[j] := arr[j + 1];
+            arr[j + 1] := temp;
+         end;
+end;
+
+procedure DisplayArr(var arr: IntArray);   
+
+begin
+   for i := 1 to size do
+      write(randomArr[i], ' ');
+   writeln;
 end;
 
 begin
    RandomIntArr(randomArr);
-   writeln(randomArr[0], ' ', randomArr[50]);
+   DisplayArr(randomArr);
+
+   SortArr(randomArr);
+   DisplayArr(randomArr);
 end. 
