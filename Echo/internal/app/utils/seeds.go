0eff5296e04942ee8ec3d586316b6604fa30fe1a
Vitaliy Kazakevich <vitaliy.dev@icloud.com>
2025-04-25 17:21:36 +0200

    Fix seeds

--- Echo/internal/app/utils/seeds.go
+++ Echo/internal/app/utils/seeds.go
@@ -7,9 +7,9 @@ import (
 
 func FillDBSeeds(db *gorm.DB) {
 	data := []models.WeatherData{
-		{City: "Kraków", Temperature: "25", Time: "2025-04-24 12:00"},
-		{City: "Kraków", Temperature: "10", Time: "2025-04-24 08:00"},
-		{City: "Kraków", Temperature: "8", Time: "2025-04-24 03:00"},
+		{City: "krakow", Temperature: "25", Time: "2025-04-24 12:00"},
+		{City: "krakow", Temperature: "10", Time: "2025-04-24 08:00"},
+		{City: "krakow", Temperature: "8", Time: "2025-04-24 03:00"},
 	}
 
 	db.Create(data)
