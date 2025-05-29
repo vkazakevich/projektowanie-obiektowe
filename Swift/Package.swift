ef1ae57371de13e2e6f36aa3a84dc37264a80f1d
Vitaliy Kazakevich <vitaliy.dev@icloud.com>
2025-05-29 15:50:51 +0200

    Init Vapor project

---
+++ Swift/Package.swift
@@ -0,0 +1,47 @@
+// swift-tools-version:6.0
+import PackageDescription
+
+let package = Package(
+    name: "Market",
+    platforms: [
+       .macOS(.v13)
+    ],
+    dependencies: [
+        // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.115.0"),
+        // 🗄 An ORM for SQL and NoSQL databases.
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.9.0"),
+        // 🪶 Fluent driver for SQLite.
+        .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.6.0"),
+        // 🍃 An expressive, performant, and extensible templating language built for Swift.
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.3.0"),
+        // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "Market",
+            dependencies: [
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
+                .product(name: "Leaf", package: "leaf"),
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+        .testTarget(
+            name: "MarketTests",
+            dependencies: [
+                .target(name: "Market"),
+                .product(name: "VaporTesting", package: "vapor"),
+            ],
+            swiftSettings: swiftSettings
+        )
+    ]
+)
+
+var swiftSettings: [SwiftSetting] { [
+    .enableUpcomingFeature("ExistentialAny"),
+] }
--- Swift/README.md
+++ Swift/README.md
@@ -1,7 +1,26 @@
 # Vapor (Swift)
 
+## Progress
+
 - 3.0 Create a controller with Products model compliant with CRUD in Fluent ORM
 - 3.5 Create templates in Leaf
 - 4.0 Create a second model and Categories controller with relationships
 - 4.5 Use Redis for data storage
 - 5.0 Deploy the application on Heroku
+
+## Getting Started
+
+To build the project using the Swift Package Manager, run the following command in the terminal from the root of the project:
+```bash
+swift build
+```
+
+To run the project and start the server, use the following command:
+```bash
+swift run
+```
+
+To execute tests, use the following command:
+```bash
+swift test
+```
