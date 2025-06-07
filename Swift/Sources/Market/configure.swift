a8b844255a86196cabfed7e6ed535f0798631144
Vitaliy Kazakevich <vitaliy.dev@icloud.com>
2025-06-08 01:26:04 +0200

    Use Heroku Redis

--- Swift/Sources/Market/configure.swift
+++ Swift/Sources/Market/configure.swift
@@ -10,7 +10,11 @@ public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.redis.configuration = try RedisConfiguration(hostname: "localhost")
+    if let redisURL = Environment.get("REDIS_URL") {
+        app.redis.configuration = try RedisConfiguration(url: redisURL)
+    } else {
+        app.redis.configuration = try RedisConfiguration(hostname: "localhost")
+    }
 
     app.databases.use(DatabaseConfigurationFactory.sqlite(.file("db.sqlite")), as: .sqlite)
 
